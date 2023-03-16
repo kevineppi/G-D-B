be9e8415929bfcaec5944f753aa537a6d2715b69
kevineppi <107027586+kevineppi@users.noreply.github.com>
2023-03-16 11:19:23 +0100

    1 file modified

--- index.js
+++ index.js
@@ -22,7 +22,88 @@ document.getElementById("app").innerHTML =
 console.log(user);
 console.log(user1);
 
-function(schedule)
-{
-this.schedule = schedule;
+
+//Aufhabe 2
+let schedule = {};
+let schedule2 = {};
+schedule2["8:30"] = "get up";
+function isEmpty(obj) {
+  for(let key in obj) {
+    if(obj.hasOwnProperty(key))
+      return false;
+  }
+  return true;
 }
+console.log(isEmpty(schedule));
+console.log(isEmpty(schedule2));
+
+
+//Aufgabe 3
+
+const usera3 = {name : "John"};
+console.log(usera3.name);
+usera3.name = "Adilette";
+console.log(usera3.name);
+
+
+//Aufgabe 4
+
+let salaries = 
+{
+John : 100,
+Ann : 160,
+Eppensteiner : 130
+};
+
+function istleer(obj)
+{
+  let sum = 0;
+    for(let key in obj) 
+    {
+      if(obj.hasOwnProperty(key))
+        
+          for (let key in obj) { // Schleife durch alle Schlüssel des Objekts
+            sum += obj[key]; // Summieren jedes Werts auf die Variable
+          }
+          return  "sum =" + " " + sum;        
+    }
+    return false;
+};
+console.log(istleer(salaries));
+
+
+//Aufgabe 5
+
+let menu = 
+{
+  width: 200,
+  length : 300,
+  title : "My menu"
+};
+function multiplyNumeric(obj)
+{
+  for (let key in obj) 
+  { 
+    if(typeof obj[key] === 'number')
+    {
+         obj[key] *= 2;
+    }
+    else
+    {
+      obj.key = obj.key;
+    }
+  }
+}
+multiplyNumeric(menu);
+console.log(menu.width);
+console.log(menu.length);
+console.log(menu.title);
+
+//Aufgabe 6
+
+let user6
+{
+  name6 = "John",
+  go: function()) {console.log(this.name)} 
+}
+
